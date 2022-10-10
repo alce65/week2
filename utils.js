f525a51082d73e136c544f85a886902114cf0938
Alejandro Cerezo <alce65@hotmail.es>
2022-10-10 16:18:08 +0200

    Update utils

--- utils.js
+++ utils.js
@@ -1,3 +1,10 @@
+// export function bar () {
+//     const x = 22 * 2;
+//     if (x > 12) {
+//         console.log(x);
+//     }
+// };
+
 export const foo = () => {
     const x = 22 * 2;
     if (x > 12) {
