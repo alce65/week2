9e081371770980a9fd5082f206f5116d5ea8696b
Alejandro Cerezo <alce65@hotmail.es>
2022-10-13 10:01:13 +0200

    Add test coverage

--- testing/sample.test.js
+++ testing/sample.test.js
@@ -1,4 +1,4 @@
-import { add } from './sample.js';
+import { add, subtraction } from './sample.js';
 
 describe('Testing add', () => {
     test('if a = 2 and b = 2 add should be', () => {
@@ -11,5 +11,24 @@ describe('Testing add', () => {
         let a = 2;
         let b = 2;
         expect(add(a, b)).toBeLessThan(5);
+    });
+
+    test('if a = 0 , return should be "Error"', () => {
+        let a = 0;
+        let b = 2;
+        expect(add(a, b)).toBe('Error');
+    });
+    test('if b = 0 , return should be "Error"', () => {
+        let a = 8;
+        let b = 5;
+        expect(add(a, b)).toBe('Error');
+    });
+});
+
+describe('Testing subtraction', () => {
+    test('if a = 2 and b = 2 result should be 0', () => {
+        let a = 2;
+        let b = 2;
+        expect(subtraction(a, b)).toBe(0);
     });
 });
